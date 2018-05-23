15bda089c256f716537e84111b3caa5d745945e1
Andi Hofmann <andreas-h-online@web.de>
2018-05-23 10:13:15 +0200

    Aufgabe 2 teilweise

--- src/main/java/ueb08/TweetSammlungImpl.java
+++ src/main/java/ueb08/TweetSammlungImpl.java
@@ -4,9 +4,12 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
+import java.util.*;
 
 public class TweetSammlungImpl implements TweetSammlung{
+
+    List<String> tweets = new LinkedList<>();
+    Map<String, Integer> counter = new HashMap<>();
 
     @Override
     public void setStopwords(File file) throws FileNotFoundException {
@@ -15,7 +18,14 @@ public class TweetSammlungImpl implements TweetSammlung{
 
     @Override
     public void ingest(String tweet) {
+        tweets.add(tweet);
 
+        for(String s : TweetSammlung.tokenize(tweet)){
+            if(counter.containsKey(s))
+                counter.put(s, counter.get(s) + 1);
+            else
+                counter.put(s, 1);
+        }
     }
 
     @Override
